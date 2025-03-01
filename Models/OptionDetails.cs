b2eed827d8729d4638f68bbf55efd2ec94de6086
devanshuhaldar <114529952+devanshuhaldar@users.noreply.github.com>
2025-02-28 20:51:33 -0600

    Add files via upload

---
+++ Models/OptionDetails.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace poc_generator.Models
+{
+	public class OptionDetails
+	{
+        public string Stock { get; set; }
+        public string Strategy { get; set; }
+		public string Outcome { get; set; }
+    }
+}
+
